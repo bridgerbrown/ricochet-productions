c470cacfada657e1eca45b2f4715a16c6da632e8
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-03-07 12:00:36 -0800

    removing circle bgs

--- components/home/landing.tsx
+++ components/home/landing.tsx
@@ -4,9 +4,6 @@ import Image from "next/image";
 export default function Landing(){
     return(
         <main className="h-screen w-screen pb-24 bg-slate-900 flex items-center justify-center">
-             <div className="top-24 left-12 absolute w-1/2 h-1/2 rounded-full bg-blue-900 opacity-10"></div>
-            <div className="top-72 -right-60 absolute  w-96 h-96 rounded-full bg-blue-900"></div>
-            <div className="bottom-12 right-24 absolute w-96 h-96 rounded-full bg-blue-900 opacity-5"></div>
             <div className="w-4/5 flex justify-center">
                 <div className="w-1/2 flex justify-end pr-4">
                     <Image
