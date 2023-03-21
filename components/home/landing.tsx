a0902292c6b2a0aea07d029af714c8fbb0416ccc
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-03-21 11:36:41 -0700

    landing spacing

--- components/home/landing.tsx
+++ components/home/landing.tsx
@@ -14,7 +14,7 @@ export default function Landing(){
                         className="w-48 sm:w-52 xl:w-80 object-scale-down"
                     />
                 </div>
-                <div className="sm:pr-0 lg:pr-4 flex justify-center flex-col space-y-2 xl:space-y-4 text-xl w-1/2 sm:px-0 lg:px-4">
+                <div className="sm:pr-0 lg:pr-4 mr-0 xl:mr-28 flex justify-center flex-col space-y-2 xl:space-y-4 text-xl w-1/2 sm:px-0 lg:px-4">
                     <h1 className="text-blue-300 font-bold text-2xl sm:text-3xl xl:text-5xl">Ricochet Productions</h1>
                     <h2 className="sm:text-lg md:text-2xl text-lg text-slate-200">Storytelling through audio</h2>
                     <h3 className="text-slate-400 font-light text-base md:text-lg sm:text-base w-9/12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, doloribus molestias! Vero consectetur laudantium cumque.</h3>
