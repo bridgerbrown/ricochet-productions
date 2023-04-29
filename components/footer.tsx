45b4751814c3bcdb968860e879934e8abb2d4b20
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-04-29 11:56:08 -0700

    footer bg fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import Link from "next/link";
 
 export default function Footer(){
-
     return(
-        <footer className="bg-[#070c1c] text-slate-100 font-base tracking-wide text-base py-12 flex justify-center items-center absolute bottom-0 w-screen">
+        <footer className="text-slate-100 font-base tracking-wide text-base py-12 flex justify-center items-center absolute bottom-0 w-screen">
             <a href="http://www.Ricochet.com"
                 className="hover:opacity-90 hover:transition"
             >
